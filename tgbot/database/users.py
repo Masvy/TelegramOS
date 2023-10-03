f0306217304c819e9f45a5cdbf33c825c06013a6
Masvy <masvy.435@gmail.com>
2023-10-03 21:20:00 +0300

    feat: write new functions

--- tgbot/database/users.py
+++ tgbot/database/users.py
@@ -17,3 +17,35 @@ async def count_user_ids(session_maker: sessionmaker):
             result = await session.execute(select(profile_table.c.user_id))
             count_user_ids = [row[0] for row in result.all()]
             return count_user_ids
+
+
+async def show_user_ids(session_maker: sessionmaker):
+    async with session_maker() as session:
+        async with session.begin():
+            result = await session.execute(select(profile_table.c.user_id))
+            user_ids = [row[0] for row in result]
+            return user_ids
+
+
+async def show_first_names(session_maker: sessionmaker):
+    async with session_maker() as session:
+        async with session.begin():
+            result = await session.execute(select(profile_table.c.name))
+            first_names = [row[0] for row in result]
+            return first_names
+
+
+async def show_user_names(session_maker: sessionmaker):
+    async with session_maker() as session:
+        async with session.begin():
+            result = await session.execute(select(profile_table.c.user_name))
+            user_names = [row[0] for row in result]
+            return user_names
+
+
+async def show_registration_date(session_maker: sessionmaker):
+    async with session_maker() as session:
+        async with session.begin():
+            result = await session.execute(select(profile_table.c.registration_date))
+            registration_date = [row[0] for row in result]
+            return registration_date
